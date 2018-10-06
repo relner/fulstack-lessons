f35ad17b095ac293eec2eeec3e15237fd0a65474
“relner” <vladimir@relner.com>
2018-10-07 00:50:32 +0300

    add bcryption and register user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,7 @@
+const bcrypt = require('bcryptjs')
+const User = require('../models/User')
+
+/*
 module.exports.login = function(req, res){
     res.status(200).json({
         login: {
@@ -7,8 +11,46 @@ module.exports.login = function(req, res){
     })
 }
 
-module.exports.register = function(req, res){
-    res.status(200).json({
-        register: 'from controller'
+module.exports.register = async function(req, res){
+
+    const user = new User({
+        email: req.body.email,
+        password: req.body.password
     })
+
+    user.save().then(() => console.log('user created'))
+
+}
+*/
+
+module.exports.login = function(req, res){
+    
+}
+
+module.exports.register = async function(req, res){
+
+    const candidate = await User.findOne({email: req.body.email})
+
+    if(candidate){
+        //user exist in db
+        res.status(409).json({
+            message: 'User already exist'
+        })
+    } else {
+        //add new user
+        const salt = bcrypt.genSaltSync(10)
+        const password = req.body.password
+
+        const user = new User({
+            email: req.body.email,
+            password: bcrypt.hashSync(password, salt)
+        })
+        try {
+            await user.save()
+            res.status(201).json(user)
+        } catch (error) {
+            
+        }
+    }
+    
 }
