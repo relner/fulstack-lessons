0020c5a01acaed902ca57d427455df3921bcd891
“relner” <vladimir@relner.com>
2018-10-08 00:20:38 +0300

    remove not used code

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,28 +3,6 @@ const User = require('../models/User')
 const jwt = require('jsonwebtoken')
 const keys = require('../config/keys')
 
-/*
-module.exports.login = function(req, res){
-    res.status(200).json({
-        login: {
-            email: req.body.email,
-            password: req.body.password
-        }
-    })
-}
-
-module.exports.register = async function(req, res){
-
-    const user = new User({
-        email: req.body.email,
-        password: req.body.password
-    })
-
-    user.save().then(() => console.log('user created'))
-
-}
-*/
-
 module.exports.login = async function(req, res){
 
     const candidate = await User.findOne({email: req.body.email})
