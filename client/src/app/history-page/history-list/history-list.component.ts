3dd215468db06a9e5d9d062896484987ddd7c3c8
“relner” <vladimir@relner.com>
2018-10-26 19:28:48 +0300

    View orders in history

--- client/src/app/history-page/history-list/history-list.component.ts
+++ client/src/app/history-page/history-list/history-list.component.ts
@@ -1,18 +1,42 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef, OnDestroy, AfterViewInit } from '@angular/core';
 import { Order } from 'src/app/shared/interfaces';
+import { MaterialInstance, MaterialService } from 'src/app/shared/classes/material.service';
 
 @Component({
   selector: 'app-history-list',
   templateUrl: './history-list.component.html',
   styleUrls: ['./history-list.component.css']
 })
-export class HistoryListComponent implements OnInit {
+export class HistoryListComponent implements OnDestroy, AfterViewInit {
 
   @Input() orders: Order[]
+  @ViewChild('modal') modalRef: ElementRef
 
-  constructor() { }
+  modal: MaterialInstance
 
-  ngOnInit() {
+  selectedOrder: Order
+
+  computePrice(order: Order): number{
+    return order.list.reduce((total, item) => {
+      return total += item.quantity * item.cost
+    }, 0)
+  }
+
+  selectOrder(order: Order){
+    this.selectedOrder = order
+    this.modal.open()
+  }
+
+  closeModal() {
+    this.modal.close()
+  }
+
+  ngAfterViewInit(): void {
+    this.modal = MaterialService.initModal(this.modalRef)
+  }
+
+  ngOnDestroy(): void {
+    this.modal.destroy()
   }
 
 }
